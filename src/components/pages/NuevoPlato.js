c34a5e4e6d9bae81460bf09067f56a5a0c4840d4
GianDvlpr <gian_franco_096@hotmail.com>
2022-04-25 13:59:00 -0500

    Leyendo datos y validando con Formik y Yup

--- src/components/pages/NuevoPlato.js
+++ src/components/pages/NuevoPlato.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import { useFormik } from 'formik'
+import * as Yup from 'yup';
 
 const NuevoPlato = () => {
+
+    //Validar y lectura de los datos del formulario
+    const formik = useFormik({
+        initialValues: {
+            nombre: '',
+            precio: '',
+            categoria: '',
+            imagen: '',
+            descripcion: ''
+        },
+        validationSchema: Yup.object({
+            nombre: Yup.string()
+                .min(3, 'Los platos deben de tener como minimo 3 carácteres')
+                .required('El nombre del platillo es obligatorio'),
+            precio: Yup.number()
+                .min(1, 'Se debe de agregar un precio')
+                .required('El precio es obligatorio'),
+            categoria: Yup.string()
+                .required('La categoria es obligatoria'),
+            descripcion: Yup.string()
+                .min(13, 'La descripción debe de tener como minimo 13 carácteres')
+                .required('La descripción es obligatoria'),
+        }),
+        onSubmit: (data) => {
+            console.log(data)
+        }
+    })
+
     return (
         <>
             <h1 className="text-3xl font-light mb-4">Nuevo Plato</h1>
 
             <div className="flex justify-center mt-10">
                 <div className="w-full max-w-3xl">
-                    <form>
+                    <form onSubmit={formik.handleSubmit}>
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">Nombre</label>
                             <input
@@ -15,8 +45,18 @@ const NuevoPlato = () => {
                                 className="shadow appareance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 type="text"
                                 placeholder="Nombre Plato"
+                                value={formik.values.nombre}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
                         </div>
+
+                        {formik.touched.nombre && formik.errors.nombre ? (
+                            <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
+                                <p className="font-bold">Hubo un error:</p>
+                                <p>{formik.errors.nombre}</p>
+                            </div>
+                        ) : null}
 
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="precio">Precio</label>
@@ -26,8 +66,18 @@ const NuevoPlato = () => {
                                 type="number"
                                 placeholder="$10.00"
                                 min="0"
+                                value={formik.values.precio}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
                         </div>
+
+                        {formik.touched.precio && formik.errors.precio ? (
+                            <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
+                                <p className="font-bold">Hubo un error:</p>
+                                <p>{formik.errors.precio}</p>
+                            </div>
+                        ) : null}
 
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="categoria">Categoria</label>
@@ -35,6 +85,9 @@ const NuevoPlato = () => {
                                 id="categoria"
                                 name="categoria"
                                 className="shadow appareance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                value={formik.values.categoria}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             >
                                 <option value="">-- Seleccione --</option>
                                 <option value="desayuno">Desayuno</option>
@@ -46,12 +99,22 @@ const NuevoPlato = () => {
                             </select>
                         </div>
 
+                        {formik.touched.categoria && formik.errors.categoria ? (
+                            <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
+                                <p className="font-bold">Hubo un error:</p>
+                                <p>{formik.errors.categoria}</p>
+                            </div>
+                        ) : null}
+
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="imagen">Imagen</label>
                             <input
                                 id="imagen"
                                 className="shadow appareance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 type="file"
+                                value={formik.values.imagen}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             />
                         </div>
 
@@ -62,8 +125,18 @@ const NuevoPlato = () => {
                                 className="shadow appareance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-40"
                                 type="text"
                                 placeholder="Descripción del plato"
+                                value={formik.values.descripcion}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
                             ></textarea>
                         </div>
+
+                        {formik.touched.descripcion && formik.errors.descripcion ? (
+                            <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 mb-5" role="alert">
+                                <p className="font-bold">Hubo un error:</p>
+                                <p>{formik.errors.descripcion}</p>
+                            </div>
+                        ) : null}
 
                         <input
                             type="submit"
