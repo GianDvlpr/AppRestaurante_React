671c121cf48f1ee4048288fff1af0bbc3faddb5f
GianDvlpr <gian_franco_096@hotmail.com>
2022-04-25 21:01:20 -0500

    Agregando una barra de porcentaje para la subida de imagenes

--- src/components/pages/NuevoPlato.js
+++ src/components/pages/NuevoPlato.js
@@ -177,6 +177,18 @@ const NuevoPlato = () => {
                             />
                         </div>
 
+                        {upload && (
+                            <div className="h-12 relative w-full border">
+                                <div className="bg-green-500 absolute left-0 top-0 text-white px-2 text-sm h-12 flex items-center" style={{width: `${progress}%`}}>
+                                    {progress}%
+                                </div>
+                            </div>
+                        )}
+
+                        {urlImg && (
+                            <p className="bg-green-500 text-white p-3 text-center my-5">La imagen se subió correctamente</p>
+                        )}
+
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="descripcion">Descripción</label>
                             <textarea
